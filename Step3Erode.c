c043f6c186ae182c4c41aa5d30ed4ccf880aa8d9
Adrian Maciejewski <adrianmaciejewski2001@gmail.com>
2025-09-26 15:58:16 +0200

    initial working shape

--- Step3Erode.c
+++ Step3Erode.c
@@ -1,16 +1,29 @@
 #include "Step3Erode.h"
 #include <stdio.h>
 
+const char erosionShape[erosionShapeSize][erosionShapeSize] = {
+  {0,1,0},
+  {1,1,1},
+  {0,1,0}
+};
+
 char isOutOfBounds(int x, int y) {
     return x<0 || x>=BMP_WIDTH || y<0 || y>=BMP_HEIGTH;
 }
 
 char shouldBeEroded(int x, int y, unsigned char binaryImage[BMP_WIDTH][BMP_HEIGTH]) {
-    return
-        (!isOutOfBounds(x-1, y) && !binaryImage[x-1][y]) ||
-        (!isOutOfBounds(x+1, y) && !binaryImage[x+1][y]) ||
-        (!isOutOfBounds(x, y-1) && !binaryImage[x][y-1]) ||
-        (!isOutOfBounds(x, y-1) && !binaryImage[x][y+1]);
+    for (int i=-erosionShapeDiameter; i<=erosionShapeDiameter; i++) {
+        for (int j=-erosionShapeDiameter; j<=erosionShapeDiameter; j++) {
+            if (erosionShape[i+erosionShapeDiameter][j+erosionShapeDiameter]==0) continue; // don't care about the central pixel
+
+            int neighborX = x + i;
+            int neighborY = y + j;
+            if (!isOutOfBounds(neighborX, neighborY) && !binaryImage[neighborX][neighborY]) {
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
 
 char erode(unsigned char binaryImage[BMP_WIDTH][BMP_HEIGTH], unsigned char outputImage[BMP_WIDTH][BMP_HEIGTH]) {
